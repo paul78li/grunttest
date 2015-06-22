09901d0d5ee02b8fd8febb7a53ca8daf39d713f6
paulli <liyou78@163.com>
2015-06-22 21:48:12 +0800

    test

--- event.js
+++ event.js
@@ -13,24 +13,45 @@ Bell.prototype.ring = function(){
 }
 
 function student(){
-    console.log('Listen');
+    console.log('student');
 }
 
 function teacher(){
-    console.log('go');
+    console.log('teacher');
 }
 
 function master(){
-    console.log('go master');
+    console.log('master');
 }
 
 var bell = new Bell();
+//默认绑定10个事件
+bell.setMaxListeners(5);
+
+bell.on('newListener',function(e,func){
+    console.log('newListener'+e+func);
+});
+bell.on('removeListener',function(e,func){
+    console.log('removeListener'+e+func);
+});
+
 //addListener on 作用一样
 bell.addListener('ring',student);
 bell.on('ring',teacher);
 bell.once('ring',master);
+
+
+
+console.log(EventEmitter.listenerCount(bell,'ring'));
 console.log(bell.listeners('ring'));
 bell.ring();
 console.log(bell.listeners('ring'));
 bell.ring();
 console.log(bell.listeners('ring'));
+
+bell.removeListener('ring',teacher);
+bell.ring();
+
+bell.removeAllListeners();
+console.log('after remove all')
+bell.ring();
