f8981b9bb1e02c19ac3afdeebed5e52e452b1992
paulli <liyou78@163.com>
2015-06-22 22:41:16 +0800

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var area = require('./area');
 console.log(area.cal(3,3));
 console.log(area.name);
-
-var Person = require('./person');
+//require 首先在node_modules中加载
+var Person = require('person');
 var person = new Person('TEST1');
 var person2 = new Person('DDD');
 console.log(person.getName());
