bc77c293cdc1f432c2d5803f153c49b610579c21
paulli <liyou78@163.com>
2015-06-22 21:02:36 +0800

    test

---
+++ console.js
@@ -0,0 +1,20 @@
+console.time('l1');
+console.log('hi');
+console.timeEnd('l1');
+
+console.error('err');
+console.warn('warn');
+
+//console.trace();
+//console.trace('tracename');
+
+
+console.dir(console);
+
+//console.assert(1==1,'show')
+//console.assert(1==2,'show 1==2 ,not equal')
+
+//标准输出
+//node console.js 1>standard.log
+//错误输出
+//node console.js 1>errout.log
