4f45139359261575dafe70e768e89ee9fd7ab18d
paulli <liyou78@163.com>
2015-06-22 22:12:02 +0800

    test

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var area = require('./area');
 console.log(area.cal(3,3));
 console.log(area.name);
 
-var Person = require('./person')
+var Person = require('./person');
 var person = new Person('TEST1');
+var person2 = new Person('DDD');
 console.log(person.getName());
 person.setName('test');
+person.name = 'hijk';
 console.log(person.getName());
+
+console.log('--------------');
+//定义在原型上，对.name直接修改值，并不能修改原型上的值
+console.log(person2.getName())
+console.log(person.getName())
